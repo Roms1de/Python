ee85dde4ff6bef5c2f8b1d340a64d8194998e2e9
Roman <romantsy21@gmail.com>
2022-12-24 11:35:43 +0300

    count and max summ

--- Git python.py
+++ Git python.py
@@ -1,3 +1,6 @@
+import math
+
+
 def bubble_sort(list):
     for i in range(len(list)):
         for j in range(len(list) - i - 1):
@@ -8,4 +11,13 @@ def bubble_sort(list):
 
 # list random numbers
 list = [345, 4556, 67, 43, 567, 234, 67, 34, 567, 234, 567, 78678, 567567, 456456, 34, 45665756, 345, 1, 567567567, 7686, 56546]
-count = 0
+new_list = bubble_sort(list)
+count, summ = 0, 0
+maxx = -math.e
+for i in range(len(new_list)):
+    if new_list[i] < 10000:
+        count += 1
+        summ += new_list[i]
+        maxx = max(maxx, summ)
+
+print(count, maxx, sep="")
